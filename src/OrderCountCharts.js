8b001d8b087ffbd4d486ec2e762ff1a9bb15bd56
Islam <islam.sayed8@gmail.com>
2018-10-08 01:43:46 +0200

    init first pie

--- src/OrderCountCharts.js
+++ src/OrderCountCharts.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react'
+import {
+    VictoryPie,
+    VictoryChart,
+    VictoryAxis,
+    VictoryTheme
+} from 'victory';
 
 class OrderCountCharts extends Component {
     render() {
-        return(
+        return (
             <div>
+                <VictoryPie
+                    data={[
+                        { y: this.props.countPerPayment("Cash"), label: "Cash" },
+                        { y: this.props.countPerPayment("KNET"), label: "KNET" },
+                        { y: this.props.countPerPayment("CreditCard"), label: "Credit Card" }
+                    ]}
+                    height={180}
+                    colorScale="warm"
+                    labelRadius={45}
+                    style={{ labels: { fill: "black", fontSize: 8 } }}
+                />
+
 
             </div>
         )
