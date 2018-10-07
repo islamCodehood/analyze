ea418c54236df477568e5d43741518970c7ffed9
Islam <islam.sayed8@gmail.com>
2018-10-08 01:42:42 +0200

    Add getCountgetCount func

--- src/App.js
+++ src/App.js
@@ -5,14 +5,26 @@ import OrderCountCharts from './OrderCountCharts'
 import RevenueCharts from './RevenueCharts'
 import TimeSeriesCharts from './TimeSeriesCharts'
 class App extends Component {
+  componentDidMount() {
+    console.log(this.state.data.filter(order => (new Date(order.orderdate).getHours()) - 2 > 20))
+  }
   state = {
     data
   }
+  //get order count per payment method.
+  getCountPerPayment = (paymentMethod) => {
+    return this.state.data.filter(order => order.paymentMethod === paymentMethod).length
+  }
+
+
+
   render() {
     return (
       <div className="App">
+        <h1>Analyze</h1>
         <OrderCountCharts
           data={this.state.data}
+          countPerPayment={this.getCountPerPayment}
         />
         <RevenueCharts 
           data={this.state.data}
