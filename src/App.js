2a0818ceddb93526a4afeae2fa7744f8adeeb6a1
Islam <islam.sayed8@gmail.com>
2018-10-08 14:07:34 +0200

    Import crossfilter and make dims

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import data from './data'
+import crossfilter from 'crossfilter'
 import PropTypes from 'prop-types'
 import OrderCountCharts from './OrderCountCharts'
 import RevenueCharts from './RevenueCharts'
@@ -8,12 +9,19 @@ import TimeSeriesCharts from './TimeSeriesCharts'
 
 class App extends Component {
   componentDidMount() {
-    this.setState({
+    /* this.setState({
       data
-    })
+    }) */
+    const facts = crossfilter(this.state.data)
+    orderAmountDim = facts.dimension(d => d.orderAmount)
+    orderdateDim = facts.dimension(d => d.orderdate)
+    paymentMethodDim = facts.dimension(d => d.paymentMethod)
+    branchDim = facts.dimension(d => d.branch)
+    deliveryAreaDim = facts.dimension(d => d.deliveryArea)
+    //console.log(crossfilter(facts).groupAll().reduceSum(function(d) {return parseFloat(d.orderAmount.replace(/[^0-9.-]+/g, ''))}).value())
   }
   state = {
-    data: []
+    data
   }
   //get order count per payment method.
   getCountPerPayment = paymentMethod => 
@@ -21,13 +29,14 @@ class App extends Component {
   
 
   getCountPerTime = (startHour, endHour) => 
-    this.state.data.filter(order => (new Date(order.orderdate).getHours()) - 2 >= startHour
+    this.state.data.filter(order => (new Date(order.orderdate).getHours()) >= startHour
      && (new Date(order.orderdate).getHours()) - 2 < endHour).length
   
   getCountPerSize = (startAmount, endAmount) =>
   this.state.data.filter(order => ((parseFloat(order.orderAmount.replace(/[^0-9.-]+/g, '')) >= startAmount)
     && (parseFloat(order.orderAmount.replace(/[^0-9.-]+/g, '')) < endAmount))
     || (parseFloat(order.orderAmount.replace(/[^0-9.-]+/g, '')) > startAmount)).length
+
 
   render() {
     return (
