cbfe158c3ac13367b6cd06a0c6568148ef72aa79
Islam <islam.sayed8@gmail.com>
2018-10-08 11:03:31 +0200

    Add PropTypes and getCountPerSize

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import data from './data'
+import PropTypes from 'prop-types'
 import OrderCountCharts from './OrderCountCharts'
 import RevenueCharts from './RevenueCharts'
 import TimeSeriesCharts from './TimeSeriesCharts'
+
 class App extends Component {
   componentDidMount() {
-    console.log(this.getCountPerTime(20, 24) + this.getCountPerTime(0, 6))
+    this.setState({
+      data
+    })
   }
   state = {
-    data
+    data: []
   }
   //get order count per payment method.
   getCountPerPayment = paymentMethod => 
@@ -20,6 +24,10 @@ class App extends Component {
     this.state.data.filter(order => (new Date(order.orderdate).getHours()) - 2 >= startHour
      && (new Date(order.orderdate).getHours()) - 2 < endHour).length
   
+  getCountPerSize = (startAmount, endAmount) =>
+  this.state.data.filter(order => ((parseFloat(order.orderAmount.replace(/[^0-9.-]+/g, '')) >= startAmount)
+    && (parseFloat(order.orderAmount.replace(/[^0-9.-]+/g, '')) < endAmount))
+    || (parseFloat(order.orderAmount.replace(/[^0-9.-]+/g, '')) > startAmount)).length
 
   render() {
     return (
@@ -29,6 +37,7 @@ class App extends Component {
           data={this.state.data}
           countPerPayment={this.getCountPerPayment}
           countPerTime={this.getCountPerTime}
+          countPerAmount={this.getCountPerSize}
         />
         <RevenueCharts 
           data={this.state.data}
@@ -39,6 +48,12 @@ class App extends Component {
       </div>
     );
   }
+}
+
+App.propTypes = {
+  data: PropTypes.array,
+  countPerPayment: PropTypes.func,
+  countPerTime: PropTypes.func
 }
 
 export default App;
