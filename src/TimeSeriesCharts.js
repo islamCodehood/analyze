f4c4ffac82881b6a950c73a6e5577e238feb7400
Islam <islam.sayed8@gmail.com>
2018-10-10 09:17:49 +0200

    Add VictoryLine comp

--- src/TimeSeriesCharts.js
+++ src/TimeSeriesCharts.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
+import {
+    VictoryLine,
+    VictoryChart
+} from 'victory';
+import PropTypes from 'prop-types'
 
 class TimeSeriesCharts extends Component {
     render() {
-        return(
+        return (
             <div>
-                
+                <VictoryChart
+                >
+                    <VictoryLine
+                        style={{
+                            data: { stroke: "#c43a31", strokeWidth: 1 },
+
+                        }}
+                        data={this.props.orderDateDim.group().all().map(date => { return { x: date.key, y: date.value } })}
+                        animate={{
+                            duration: 2000,
+                            onLoad: { duration: 2000 }
+                        }}
+                    />
+                </VictoryChart>
             </div>
         )
     }
