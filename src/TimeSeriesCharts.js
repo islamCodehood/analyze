e13df9815407b8a34be59dbc24b8178eb851e02e
Islam <islam.sayed8@gmail.com>
2018-10-14 15:35:32 +0200

    Add charts for month

--- src/TimeSeriesCharts.js
+++ src/TimeSeriesCharts.js
@@ -1,16 +1,42 @@
 import React, { Component } from "react";
-import { VictoryLine, VictoryChart, VictoryBrushContainer } from "victory";
+import { VictoryLine, VictoryChart, VictoryBrushContainer, VictoryZoomContainer, VictoryAxis } from "victory";
 //import PropTypes from 'prop-types'
 
 class TimeSeriesCharts extends Component {
+
+  state = {
+    selectedDomain: {},
+    zoomDomain: {}
+  }
+
   handleChange = (domain, props) => {
     this.props.handleChartChange(domain.x, props.name);
+    console.log(domain)
   };
+
+  handleResetClick = (e) => {
+    if (e.target.text === "Reset All") {
+      this.props.resetAll()
+    } else {
+      this.props.resetDim(e.target.parentElement.id)
+    }
+    
+  }
+
+
+
 
   render() {
     return (
       <div className="card-container">
       <div className="time-series-card">
+      <h2 className="time-series-title">Order Count / Day</h2>
+      <br/>
+          <br/>
+          <div className="btn-group">
+
+          <a href="#!" className="reset" onClick={this.handleResetClick}>Reset All</a>
+          </div>
         <VictoryChart
           width={1000}
           containerComponent={
@@ -21,7 +47,7 @@ class TimeSeriesCharts extends Component {
               onBrushDomainChange={this.handleChange}
               brushStyle={{
                 stroke: "transparent",
-                fill: "red",
+                fill: "#999",
                 fillOpacity: 0.3
               }}
               name="orderDateChart"
@@ -30,7 +56,7 @@ class TimeSeriesCharts extends Component {
         >
           <VictoryLine
             style={{
-              data: { stroke: "#c43a58", strokeWidth: 1 }
+              data: { stroke: "#c43a58", strokeWidth: 2 }
             }}
             
             data={this.props.orderDateDim
@@ -47,6 +73,13 @@ class TimeSeriesCharts extends Component {
         </VictoryChart>
         </div>
         <div className="time-series-card">
+        <h2 className="time-series-title">Sales / Day</h2>
+        <br/>
+          <br/>
+          <div className="btn-group">
+
+          <a href="#!" className="reset" onClick={this.handleResetClick}>Reset All</a>
+          </div>
         <VictoryChart
         width={1000}
           containerComponent={
@@ -57,7 +90,7 @@ class TimeSeriesCharts extends Component {
               onBrushDomainChange={this.handleChange}
               brushStyle={{
                 stroke: "transparent",
-                fill: "red",
+                fill: "#999",
                 fillOpacity: 0.3
               }}
               name="orderDateChart"
@@ -66,9 +99,103 @@ class TimeSeriesCharts extends Component {
         >
           <VictoryLine
             style={{
-              data: { stroke: "#c43a58", strokeWidth: 1 }
+              data: { stroke: "#c43a58", strokeWidth: 2 },
+              
             }}
             data={this.props.orderDateDim
+              .group()
+              .reduceSum(d =>
+                parseFloat(d.orderAmount.replace(/[^0-9.-]+/g, ""))
+              )
+              .all()
+              .map(date => {
+                return { x: date.key, y: date.value };
+              })}
+            animate={{
+              duration: 2000,
+              onLoad: { duration: 2000 }
+            }}
+          />
+        </VictoryChart>
+        </div>
+        <div className="time-series-card">
+        <h2 className="time-series-title">Order Count / Month</h2>
+        <br/>
+          <br/>
+          <div className="btn-group">
+
+          <a href="#!" className="reset" onClick={this.handleResetClick}>Reset All</a>
+          </div>
+        <VictoryChart
+        width={1000}
+          containerComponent={
+            <VictoryBrushContainer
+              brushDimension="x"
+              brushDomain={{ x: [4, 6] }}
+              defaultBrushArea={"none"}
+              onBrushDomainChange={this.handleChange}
+              brushStyle={{
+                stroke: "transparent",
+                fill: "#999",
+                fillOpacity: 0.3
+              }}
+              name="orderDateChart"
+            />
+          }
+        >
+          <VictoryLine
+            style={{
+              data: { stroke: "#c43a58", strokeWidth: 2 },
+              
+            }}
+            data={this.props.orderMonthDim
+              .group()
+              .all()
+              .map(date => {
+                return { x: date.key, y: date.value };
+              })}
+            animate={{
+              duration: 2000,
+              onLoad: { duration: 2000 }
+            }}
+            animate={{
+              duration: 2000,
+              onLoad: { duration: 2000 }
+            }}
+          />
+        </VictoryChart>
+        </div>
+        <div className="time-series-card">
+        <h2 className="time-series-title">Sales / Month</h2>
+        <br/>
+          <br/>
+          <div className="btn-group">
+
+          <a href="#!" className="reset" onClick={this.handleResetClick}>Reset All</a>
+          </div>
+        <VictoryChart
+        width={1000}
+          containerComponent={
+            <VictoryBrushContainer
+              brushDimension="x"
+              brushDomain={{ x: [4, 6] }}
+              defaultBrushArea={"none"}
+              onBrushDomainChange={this.handleChange}
+              brushStyle={{
+                stroke: "transparent",
+                fill: "#999",
+                fillOpacity: 0.3
+              }}
+              name="orderDateChart"
+            />
+          }
+        >
+          <VictoryLine
+            style={{
+              data: { stroke: "#c43a58", strokeWidth: 2 },
+              
+            }}
+            data={this.props.orderMonthDim
               .group()
               .reduceSum(d =>
                 parseFloat(d.orderAmount.replace(/[^0-9.-]+/g, ""))
