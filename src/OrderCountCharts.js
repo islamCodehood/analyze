581ce27e7bad6bc878b85564a83e487cc9b1dbd7
Islam <islam.sayed8@gmail.com>
2018-10-14 15:34:37 +0200

    Add labels nd reset btns

--- src/OrderCountCharts.js
+++ src/OrderCountCharts.js
@@ -4,7 +4,9 @@ import {
   VictoryBar,
   VictoryChart,
   VictoryBrushContainer,
-  VictoryLabel
+  VictoryLabel,
+  VictoryTheme,
+  VictoryAxis
 } from "victory";
 import PropTypes from "prop-types";
 
@@ -31,11 +33,38 @@ class OrderCountCharts extends Component {
     }, 100);
   };
 
+  handleResetClick = (e) => {
+    if (e.target.text === "Reset All") {
+      this.props.resetAll()
+      /* if (document.getElementsByTagName('path')[0].style.fill !== "rgb(76, 76, 130)") {
+        console.log(this.props.style.fill) 
+      } */
+    } else {
+      this.props.resetDim(e.target.parentElement.id)
+    }
+    
+  }
+    
+  
+
+  /* handleResetClick = () => {
+
+  } */
+
   render() {
     return (
-      <div className="card-container">
-        <div id="pie-1" className="card">
+      <div className="card-container">     
+        <div id="orderCount-paymentMethod" className="card">
+          <h2>Orders Count / Payment Method</h2>
+          <br/>
+          <br/>
+          <div className="btn-group">
+          <a href="#!" className="reset" onClick={this.handleResetClick}>Reset All</a>
+          <a href="#!" className="reset" onClick={this.handleResetClick}>Reset Dimension</a>
+          </div>
+    
           <VictoryPie
+          className="pie"
             name="payMethodPieChart"
             responsive={false}
             data={this.props.paymentMethodDim
@@ -46,9 +75,9 @@ class OrderCountCharts extends Component {
               })}
             height={280}
             width={280}
-            colorScale="warm"
+            colorScale={["#3d3d42", "#008f68", "#EFBB35"]}
             labelRadius={120}
-            style={{ labels: { fill: "#fff", fontSize: 10 } }}
+            style={{ labels: { fill: "transparent", fontSize: 10 } }}
             events={[
               {
                 target: "data",
@@ -78,9 +107,10 @@ class OrderCountCharts extends Component {
                       {
                         target: "data",
                         mutation: props => {
-                          return props.style.fill === "red"
-                            ? "blue"
-                            : { style: { fill: "red" } };
+                          //console.log(typeof(props.style.fill))
+                          return props.style.fill === "#4c4c82"
+                            ? props.style.fill
+                            : { style: { fill: "#4c4c82" } };
                         }
                       }
                     ];
@@ -89,8 +119,21 @@ class OrderCountCharts extends Component {
               }
             ]}
           />
+          <div className="labels-container">
+          <div id="box-1"></div><div className="label-1">Cash</div>
+          <div id="box-2"></div><div className="label-2">Credit Card</div>
+          <div id="box-3"></div><div className="label-3">KNET</div>
+          </div>
         </div>
-        <div id="pie-2" className="card">
+        <div id="orderCount-orderTime" className="card">
+        <h2>Orders Count / Order Time</h2>
+        <br/>
+          <br/>
+          <div className="btn-group">
+
+          <a href="#!" className="reset" onClick={this.handleResetClick}>Reset All</a>
+          <a href="#!" className="reset" onClick={this.handleResetClick}>Reset Dimension</a>
+          </div>
           <VictoryPie
             responsive={false}
             data={[
@@ -121,9 +164,9 @@ class OrderCountCharts extends Component {
             ]}
             height={280}
             width={280}
-            colorScale="warm"
+            colorScale={["#c8e7b0", "#3d3d42", "#4db7ce", "#008f68"]}
             labelRadius={120}
-            style={{ labels: { fill: "#fff", fontSize: 10 } }}
+            style={{ labels: { fill: "transparent", fontSize: 10 } }}
             animate={{
               duration: 2000,
               onLoad: { duration: 3000 }
@@ -137,7 +180,6 @@ class OrderCountCharts extends Component {
                       {
                         target: "labels",
                         mutation: props => {
-                          console.log(props)
                           if (this.state.clickedPieSlice.find(slice => slice[0] === props.datum.label[0]) === undefined) {
                             this.state.clickedPieSlice.push(props.datum.label)
                             this.setState({
@@ -158,9 +200,9 @@ class OrderCountCharts extends Component {
                       {
                         target: "data",
                         mutation: props => {
-                          return props.style.fill === "red"
+                          return props.style.fill === "#4c4c82"
                             ? "blue"
-                            : { style: { fill: "red" } };
+                            : { style: { fill: "#4c4c82" } };
                         }
                       }
                     ];
@@ -169,8 +211,22 @@ class OrderCountCharts extends Component {
               }
             ]}
           />
+          <div className="labels-container-orderTime">
+          <div id="box-1-orderTime"></div><div className="label-1-orderTime">Morning</div>
+          <div id="box-2-orderTime"></div><div className="label-2-orderTime">Afternoon</div>
+          <div id="box-3-orderTime"></div><div className="label-3-orderTime">Evening</div>
+          <div id="box-4-orderTime"></div><div className="label-4-orderTime">Night</div>
+          </div>
         </div>
-        <div id="pie-3" className="card">
+        <div id="orderCount-orderSize" className="card">
+        <h2>Orders Count / Order Size</h2>
+        <br/>
+          <br/>
+          <div className="btn-group">
+
+          <a href="#!" className="reset" onClick={this.handleResetClick}>Reset All</a>
+          <a href="#!" className="reset" onClick={this.handleResetClick}>Reset Dimension</a>
+          </div>
           <VictoryPie
             responsive={false}
             innerRadius={10}
@@ -204,12 +260,13 @@ class OrderCountCharts extends Component {
             ]}
             height={280}
             width={280}
-            colorScale="warm"
+            colorScale={["#c8e7b0", "#3d3d42", "#4db7ce", "#008f68", "#EFBB35"]}
             labelRadius={120}
-            style={{ labels: { fill: "#fff", fontSize: 10 } }}
+            innerRadius={50}
+            style={{ labels: { fill: "transparent", fontSize: 10 } }}
             animate={{
-              duration: 2000,
-              onLoad: { duration: 3000 }
+              duration: 500,
+              onLoad: { duration: 800 }
             }}
             events={[
               {
@@ -240,9 +297,9 @@ class OrderCountCharts extends Component {
                       {
                         target: "data",
                         mutation: props => {
-                          return props.style.fill === "red"
+                          return props.style.fill === "#4c4c82"
                             ? "blue"
-                            : { style: { fill: "red" } };
+                            : { style: { fill: "#4c4c82" } };
                         }
                       }
                     ];
@@ -251,9 +308,27 @@ class OrderCountCharts extends Component {
               }
             ]}
           />
+          <div className="labels-container-orderAmount">
+          <div id="box-1-orderAmount"></div><div className="label-1-orderAmount">Less than $10</div>
+          <div id="box-2-orderAmount"></div><div className="label-2-orderAmount">$10 - $20</div>
+          <div id="box-3-orderAmount"></div><div className="label-3-orderAmount">$20 - $40</div>
+          <div id="box-4-orderAmount"></div><div className="label-4-orderAmount">$40 - $70</div>
+          <div id="box-5-orderAmount"></div><div className="label-5-orderAmount">$70 and more</div>
+          </div>
         </div>
-        <div id="bar-1" className="card">
-          <VictoryChart responsive={false} domainPadding={20}>
+        <div id="orderCount-branch" className="card">
+        <h2>Orders Count / Branch</h2>
+        <br/>
+          <br/>
+          <div className="btn-group">
+
+          <a href="#!" className="reset" onClick={this.handleResetClick}>Reset All</a>
+          </div>
+          <VictoryChart
+           responsive={false}
+            domainPadding={16}
+            padding={{left: 80, right: 60}}
+            >
             <VictoryBar
               horizontal
               labelComponent={
@@ -267,17 +342,17 @@ class OrderCountCharts extends Component {
                 .group()
                 .all()
                 .map(branch => {
-                  return { y: branch.value, x: branch.key, label: branch.key };
+                  return { y: branch.value, x: branch.key, label: branch.value + " Order" };
                 })}
               style={{
-                data: { fill: (d, active) => (active ? "grey" : "#C64828") },
-                labels: { fill: "#888" }
+                data: { fill: "#008f68" },
+                labels: { fill: "#666", fontSize: 14 }
               }}
               animate={{
-                duration: 2000,
-                onLoad: { duration: 3000 }
+                duration: 500,
+                onLoad: { duration: 800 }
               }}
-              barWidth={25}
+              barWidth={31}
               events={[
                 {
                   target: "data",
@@ -287,17 +362,17 @@ class OrderCountCharts extends Component {
                         {
                           target: "labels",
                           mutation: props => {
-                            if (this.state.clickedBar.includes(props.text)) {
+                            if (this.state.clickedBar.includes(props.datum.x)) {
                               this.setState(prevState => ({
                                 clickedBar: prevState.clickedBar.filter(
-                                  branch => branch !== props.text
+                                  branch => branch !== props.datum.x
                                 )
                               }));
                               this.handleBranchBarClick();
                             } else {
                               this.setState(prevState => ({
                                 clickedBar: prevState.clickedBar.concat(
-                                  props.text
+                                  props.datum.x
                                 )
                               }));
                               this.handleBranchBarClick();
@@ -307,9 +382,9 @@ class OrderCountCharts extends Component {
                         {
                           target: "data",
                           mutation: props => {
-                            return props.style.fill === "red"
+                            return props.style.fill === "#4c4c82"
                               ? "blue"
-                              : { style: { fill: "red" } };
+                              : { style: { fill: "#4c4c82" } };
                           }
                         }
                       ];
@@ -320,44 +395,66 @@ class OrderCountCharts extends Component {
             />
           </VictoryChart>
         </div>
-        <div id="bar-2" className="card">
+        <div id="orderCount-deliveryArea" className="card">
+        <h2>Orders Count / Delivery Area</h2>
+        <br/>
+          <br/>
+          <div className="btn-group">
+
+          <a href="#!" className="reset" onClick={this.handleResetClick}>Reset All</a>
+          </div>
           <VictoryChart
             responsive={false}
             containerComponent={
               <VictoryBrushContainer
                 brushDimension="x"
-                brushDomain={{ x: [9, 11] }}
+                brushDomain={{ x: [6, 14] }}
                 defaultBrushArea={"none"}
                 onBrushDomainChange={this.handleChange}
                 brushStyle={{
                   stroke: "transparent",
-                  fill: "#9995",
+                  fill: "#999",
                   fillOpacity: 0.3
                 }}
                 name="deliverAreaChart"
               />
             }
-            domainPadding={20}
+            domainPadding={9}
+            
           >
+            
             <VictoryBar
               data={this.props.deliveryAreaDim
                 .group()
                 .top(20)
                 .map(order => {
-                  return { y: order.value, x: order.key };
+                  return { y: order.value, x: order.key.substr(0, 8) };
                 })}
               style={{
                 data: { fill: (d, active) => (active ? "grey" : "#33619D") },
                 labels: { fill: "#888", fontSize: 10 }
               }}
+              barWidth={17} 
               animate={{
-                duration: 2000,
-                onLoad: { duration: 3000 }
+                duration: 500,
+                onLoad: { duration: 800 }
               }}
             />
+            <VictoryAxis
+            style={{ tickLabels: { angle: -70, fontSize: 12, padding: 25 } }}
+            
+          />
+            <VictoryAxis dependentAxis />
           </VictoryChart>
         </div>
-        <div id="bar-3" className="card">
+        <div id="orderCount-weekDay" className="card">
+        <h2>Orders Count / Week Day</h2>
+        <br/>
+          <br/>
+          <div className="btn-group">
+
+          <a href="#!" className="reset" onClick={this.handleResetClick}>Reset All</a>
+          </div>
           <VictoryChart
             responsive={false}
             containerComponent={
@@ -368,13 +465,13 @@ class OrderCountCharts extends Component {
                 onBrushDomainChange={this.handleChange}
                 brushStyle={{
                   stroke: "transparent",
-                  fill: "red",
+                  fill: "#999",
                   fillOpacity: 0.3
                 }}
                 name="orderWeekDayChart"
               />
             }
-            domainPadding={20}
+            domainPadding={10}
           >
             <VictoryBar
               data={this.props.orderWeekDayDim
@@ -399,10 +496,15 @@ class OrderCountCharts extends Component {
                 data: { fill: "#C64828" }
               }}
               animate={{
-                duration: 2000,
-                onLoad: { duration: 3000 }
+                duration: 1500,
+                onLoad: { duration: 1200 }
               }}
             />
+            <VictoryAxis
+            style={{ tickLabels: { angle: -70, fontSize: 15, padding: 32 } }}
+            
+          />
+            <VictoryAxis dependentAxis />
           </VictoryChart>
         </div>
       </div>
